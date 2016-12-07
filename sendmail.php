d0febb333a212596afc3e9055f908cdd5ed796ed
roee <roy@mbtechnologyil.com>
2016-12-07 16:40:21 +0200

    tesT

--- sendmail.php
+++ sendmail.php
@@ -7,18 +7,27 @@
  */
 require('vendor/autoload.php');
 
+$to = [$_GET['to']];
+
+if (isset($_GET['recipient']))
+{
+    $to = [ $_GET['to'] => $_GET['recipient'] ];
+}
+
+$body = $_GET['body'];
+
 // Create the Transport
 $transport = Swift_SmtpTransport::newInstance('127.0.0.1', 25)
-    ->setUsername('manyak@amentraffic.com')
-    ->setPassword('manyak');
+    ->setUsername('support@amentraffic.com')
+    ->setPassword('Abc1234');
 
 // Create the Mailer using your created Transport
 $mailer = Swift_Mailer::newInstance($transport);
 
 // Create a message
-$message = Swift_Message::newInstance('Wonderful Subject')
-    ->setFrom(array('manyak@amentraffic.com' => 'John Doe'))
-    ->setTo(array('receiver@domain.org', 'other@domain.org' => 'A name'))
+$message = Swift_Message::newInstance($subject)
+    ->setFrom(array('support@amentraffic.com' => 'George Berg'))
+    ->setTo(array($to))
     ->setBody('Here is the message itself');
 
 // Send the message
